cb74f700d7dbc0a9d9aa21ad9997dc5dab751ee6
Chin Yu <chin.yuchiuj@gmail.com>
2021-08-25 22:39:09 +0800

    add count

--- guess.py
+++ guess.py
@@ -1,13 +1,17 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
-	num = input('guess the number')
+	count += 1 # count = count + 1 
+	num = input('guess the number: ')
 	num = int(num)
 	if num == r:
 		print('bingo!!')
+		print('you have counted', count, 'time(s)')
 		break
 	elif num > r:
 		print('greater than the number')
 	elif num < r:
 		print('less than the number')
+	print('you have counted', count, 'time(s)')
